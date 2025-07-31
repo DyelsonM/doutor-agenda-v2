cca9cbbc14602449856a7328223c5cdcf81391a1
DyelsonM <dyelsonm.dev@gmail.com>
2025-07-31 13:52:06 -0300

    feat: implement appointment management with add and delete functionality, integrate calendar and popover components

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -100,7 +100,7 @@ export function AppSidebar() {
               <DropdownMenuTrigger asChild>
                 <SidebarMenuButton size="lg">
                   <Avatar>
-                    <AvatarFallback>D</AvatarFallback>
+                    <AvatarFallback>F</AvatarFallback>
                   </Avatar>
                   <div>
                     <p className="text-sm">
